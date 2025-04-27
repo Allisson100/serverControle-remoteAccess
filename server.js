c86fb191bc1b67f8628bf420af594ff3b994b87e
Allisson100 <allisson.boiane@outlook.com>
2025-04-26 22:14:11 -0300

    feat: feat: routes/crypt

--- server.js
+++ server.js
@@ -1,197 +1,89 @@
+require("dotenv").config();
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
 const cors = require("cors");
-const robot = require("robotjs");
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-  allowEIO3: true,
-});
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("OPA");
+  res.send("Server control running");
 });
 
-// Armazena a sala ativa com o estado e os sockets conectados
-let activeRoom = {
-  id: null,
-  controlAllowed: true,
-  sender: null,
-  receiver: null,
-};
+const dbMock = [
+  {
+    // gerar manualmente
+    email: "allisson@teste.com",
+    // gerar manualmente
+    password: "123",
+    // gerar manualmente
+    isUserBlocked: false,
+    // gerar manualmente
+    clientId: "123456",
 
-// CONTROLE DE LETRAS DO TECLADO
-const allowedKeys = new Set([
-  // Letras
-  ..."abcdefghijklmnopqrstuvwxyz",
+    // automatico sistema
+    ngrokUrl: null,
+    // automatico sistema
+    hash: null,
+  },
+];
 
-  // Números
-  ..."0123456789",
+app.post("/api/register-connection", async (req, res) => {
+  try {
+    const { clientId, ngrokUrl, hash } = req.body;
 
-  // Funções
-  ..."f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12".split(" "),
-
-  // Navegação / Controle
-  "enter",
-  "escape",
-  "backspace",
-  "tab",
-  "delete",
-  "insert",
-  "home",
-  "end",
-  "pageup",
-  "pagedown",
-  "up",
-  "down",
-  "left",
-  "right",
-  "space",
-  "printscreen",
-
-  // Modificadores
-  "shift",
-  "control",
-  "alt",
-  "command",
-
-  // Pontuação
-  "comma",
-  "period",
-  "slash",
-  "backslash",
-  "semicolon",
-  "quote",
-  "openbracket",
-  "closebracket",
-  "minus",
-  "equals",
-  "grave",
-]);
-
-io.on("connection", (socket) => {
-  console.log("Novo usuário conectado" + socket.id);
-
-  // O client entra na sala informada
-  socket.on("joinRoom", (roomId) => {
-    if (activeRoom.id !== roomId) {
-      socket.emit("roomNotFound");
-      return;
+    if (!clientId || !ngrokUrl || !hash) {
+      return res.status(400).json({ error: "Invalid customer datas" });
     }
-    activeRoom.receiver = socket;
-    socket.join(roomId);
-    console.log(`Client entrou na sala ${roomId}`);
 
-    activeRoom.sender.emit("clientConnected");
-  });
-
-  // O host cria a sala
-  socket.on("createRoom", (roomId) => {
-    activeRoom = {
-      id: roomId,
-      controlAllowed: true,
-      sender: socket,
-      receiver: null,
-    };
-    socket.join(roomId);
-    console.log(`Sala criada: ${roomId}`);
-  });
-
-  // Alterna o controle do mouse
-  socket.on("toggleControl", ({ roomId, allowed }) => {
-    if (activeRoom.id === roomId) {
-      activeRoom.controlAllowed = allowed;
+    // buscar no banco se o hash não é null
+    // se não for null tem que setar no banco o que esta vindo da req, pois é o primeiro acesso
+    if (!dbMock?.[0]?.hash) {
+      dbMock[0].hash = hash;
     }
-  });
 
-  // Movimento do mouse: somente se permitido
-  socket.on("moveMouse", ({ roomId, x, y }) => {
-    if (activeRoom.id === roomId && activeRoom.controlAllowed) {
-      const screenSize = robot.getScreenSize();
-      const realX = Math.floor(x * screenSize.width);
-      const realY = Math.floor(y * screenSize.height);
-      robot.moveMouse(realX, realY);
+    // Se o banco encontrar esse hash então deve compará-lo de acordo com o clientId
+    // Para saber se o cara não esta acessando de outro pc o servidor
+    if (dbMock?.find((mock) => mock?.clientId === clientId)?.hash !== hash) {
+      return res.status(401).json({ message: "Unauthorized user" });
     }
-  });
 
-  // Simular clique do mouse
-  socket.on("mouseDown", (data) => {
-    robot.mouseClick();
-  });
+    dbMock[0].ngrokUrl = ngrokUrl;
 
-  //  ### CONEXÃO HOST E CLIENT PARA STREM DO VIDEO ###
-  socket.on("offer", (offer) => {
-    socket.broadcast.emit("offer", offer);
-  });
-
-  socket.on("answer", (answer) => {
-    socket.broadcast.emit("answer", answer);
-  });
-
-  socket.on("ice-candidate", (candidate) => {
-    socket.broadcast.emit("ice-candidate", candidate);
-  });
-  //  ### CONEXÃO HOST E CLIENT PARA STREM DO VIDEO ###
-
-  // COMPARTILHA IMAGEM
-  socket.on("shareImage", ({ roomId, imageData }) => {
-    if (activeRoom.id === roomId && activeRoom.receiver) {
-      activeRoom.receiver.emit("displayImage", imageData);
-    }
-  });
-
-  // PARA DE COMPARTILHAR IMAGEM
-  socket.on("stopImageShare", ({ roomId }) => {
-    if (activeRoom.id === roomId && activeRoom.receiver) {
-      activeRoom.receiver.emit("hideImage");
-    }
-  });
-
-  // PARA COMPARTILHAMENTO DE VIDEO
-  socket.on("stopScreenSharing", ({ roomId }) => {
-    if (activeRoom.id === roomId && activeRoom.receiver) {
-      activeRoom.receiver.emit("stopStream");
-    }
-  });
-
-  // TECLADO
-  socket.on("keyboard-type", (key) => {
-    if (!key || typeof key !== "string") return;
-
-    const keyLower = key.toLowerCase();
-
-    if (allowedKeys.has(keyLower)) {
-      try {
-        robot.keyTap(keyLower);
-      } catch (error) {
-        console.error(`Erro ao digitar tecla '${keyLower}':`, error);
-      }
-    } else {
-      console.warn(`Tecla não permitida: '${key}'`);
-    }
-  });
-
-  socket.on("disconnect", () => {
-    if (
-      (activeRoom.sender && socket.id === activeRoom.sender.id) ||
-      (activeRoom.receiver && socket.id === activeRoom.receiver.id)
-    ) {
-      activeRoom = {
-        id: null,
-        controlAllowed: true,
-        sender: null,
-        receiver: null,
-      };
-    }
-  });
+    res.status(200).json({ message: "Successfully connected" });
+  } catch (error) {
+    res.status(500).json({ message: "Error to connect" });
+  }
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (dbMock?.[0]?.email !== email || dbMock?.[0]?.password !== password) {
+      return res.status(401).json({ message: "Unauthorized user" });
+    }
+
+    if (!dbMock?.[0]?.ngrokUrl) {
+      return res.status(401).json({ message: "Local server not running" });
+    }
+
+    if (dbMock?.[0]?.isUserBlocked) {
+      return res.status(401).json({ message: "User blocked" });
+    }
+
+    res.status(200).json({
+      message: "Sucesso",
+      userInfos: {
+        connectionUrl: dbMock?.[0]?.ngrokUrl,
+        isUserBlocked: dbMock?.[0]?.isUserBlocked,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Conexão registrada com sucesso" });
+  }
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
